f30d6f23ddb268c1ac66c34d5596c7d55824074b
static-debug <lpy.web@qq.com>
2025-10-31 17:10:01 +0800

    Add files via upload

---
+++ mod/src/index.js
@@ -0,0 +1,43 @@
+window.onload = () => {
+  const menuBtn = $("._header-right");
+  const contentContainer = $("._content-container");
+  const contentModal = $("._content-modal");
+  const moreBtn = $("._more");
+  const logoBtn = $("._logo")
+  const loading = $("._loading");
+
+  const parentMenu = $("._parent-menu");
+  const subMenu = $("._sub-menu");
+  const mentBack = $("._menu-back")
+
+  $(document).ready(() => {
+    contentModal.hide();
+    setTimeout(() => {
+      loading.addClass("_hidden")
+    }, 2000)
+  });
+
+  logoBtn.click(() => {
+    location.href = "https://www.tourguidetraining.ae"
+  })
+
+  menuBtn.click(() => {
+    contentContainer.removeClass("_show").addClass("_hidden");
+    contentModal.show();
+  });
+
+  contentModal.click(() => {
+    contentModal.hide();
+    contentContainer.removeClass("_hidden").addClass("_show");
+  });
+
+  moreBtn.click(() => {
+    parentMenu.removeClass("_show").addClass("_hidden")
+    subMenu.removeClass("_hidden").addClass("_show")
+  })
+
+  mentBack.click(() => {
+    parentMenu.removeClass("_hidden").addClass("_show")
+    subMenu.removeClass("_show").addClass("_hidden")
+  })
+};
