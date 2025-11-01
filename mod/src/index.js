8741f98d7ba58e4655ecb21090f02d5cbf40be69
static-debug <lpy.web@qq.com>
2025-11-01 14:51:24 +0800

    Update index.js

--- mod/src/index.js
+++ mod/src/index.js
@@ -3,41 +3,44 @@ window.onload = () => {
   const contentContainer = $("._content-container");
   const contentModal = $("._content-modal");
   const moreBtn = $("._more");
-  const logoBtn = $("._logo")
+  const logoBtn = $("._logo");
   const loading = $("._loading");
 
   const parentMenu = $("._parent-menu");
   const subMenu = $("._sub-menu");
-  const mentBack = $("._menu-back")
+  const mentBack = $("._menu-back");
+  const menuContainer = $("._menu");
 
   $(document).ready(() => {
     contentModal.hide();
     setTimeout(() => {
-      loading.addClass("_hidden")
-    }, 2000)
+      loading.addClass("_hidden");
+    }, 2000);
   });
 
   logoBtn.click(() => {
-    location.href = "https://www.tourguidetraining.ae"
-  })
+    location.href = "https://www.tourguidetraining.ae";
+  });
 
   menuBtn.click(() => {
+    menuContainer.removeClass("_hidden");
     contentContainer.removeClass("_show").addClass("_hidden");
     contentModal.show();
   });
 
   contentModal.click(() => {
+    menuContainer.addClass("_hidden");
     contentModal.hide();
     contentContainer.removeClass("_hidden").addClass("_show");
   });
 
   moreBtn.click(() => {
-    parentMenu.removeClass("_show").addClass("_hidden")
-    subMenu.removeClass("_hidden").addClass("_show")
-  })
+    parentMenu.removeClass("_show").addClass("_hidden");
+    subMenu.removeClass("_hidden").addClass("_show");
+  });
 
   mentBack.click(() => {
-    parentMenu.removeClass("_hidden").addClass("_show")
-    subMenu.removeClass("_show").addClass("_hidden")
-  })
+    parentMenu.removeClass("_hidden").addClass("_show");
+    subMenu.removeClass("_show").addClass("_hidden");
+  });
 };
